3ce13aecb20a035ecfefdcafa1a657be34d819b4
Sayed-Abulfazl <sah.sayedabulfazl@gmail.com>
2025-10-06 19:09:55 -0700

    3
    update3

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,10 +1,32 @@
 // const { mongoose } = require('mongoose'); // can not import this kind
+
 import mongoose from 'mongoose';
 
+// export const connectDB = async () => {
+//     if(mongoose.connection.readyState >= 1){
+//         return;
+//     }
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log('Conncted To MongoDB !!!')
+// }
+// above I could not connect to mongo on vercel 
+
+// now ChatGPT get a way :
+
+let cached = global.mongoose;
+
+if(!cached){
+    cached = global.mongoose = { conn: null, promise : null };
+}
+
 export const connectDB = async () => {
-    if(mongoose.connection.readyState >= 1){
-        return;
+    if(cached.conn) return cached.conn;
+
+    if(!cached.promise){
+        cached.promise = mongoose.connect(process.env.MONGO_URI).then(mongoose => {
+            return mongoose;
+        });
     }
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('Conncted To MongoDB !!!')
+    cached.conn = await cached.promise;
+    return cached.conn;
 }
